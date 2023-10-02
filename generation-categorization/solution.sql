0cba5c8f9dfe7323f456da4ba76ea9c4abda7883
DaniFuente <d.fuente@outlook.es>
2023-10-02 13:47:43 +0200

    updated generation-categorization kata

--- generation-categorization/solution.sql
+++ generation-categorization/solution.sql
@@ -1,3 +1,12 @@
--- Substitute wih your SQL
-select name, birthdate, Generation Z as generation from users
-where birthdate::year 
+select id, name, birthdate, 'Generation Z' as generation from users where date_part('year', birthdate) BETWEEN 1997 AND 2012
+union
+select id, name, birthdate, 'Millennials' as generation from users where date_part('year', birthdate) BETWEEN 1981 AND 1996
+union
+select id, name, birthdate, 'Generation X' as generation from users where date_part('year', birthdate) BETWEEN 1965 AND 1980
+union
+select id, name, birthdate, 'Baby Boomers' as generation from users where date_part('year', birthdate) BETWEEN 1946 AND 1964
+union
+select id, name, birthdate, 'Silent Generation' as generation from users where date_part('year', birthdate) BETWEEN 1928 AND 1945
+union
+select id, name, birthdate, 'Greatest Generation' as generation from users where date_part('year', birthdate) <= 1927
+order by id asc
